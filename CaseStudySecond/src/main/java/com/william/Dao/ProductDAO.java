91541da963a539fa15b0d2253fa6226a1c4e45a5
Tuan Anh <williamnguyen8888@gmail.com>
2020-11-26 09:06:13 +0700

    first commit

---
+++ CaseStudySecond/src/main/java/com/william/Dao/ProductDAO.java
@@ -0,0 +1,187 @@
+package com.william.Dao;
+
+import com.william.model.CategoryModel;
+import com.william.model.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ProductDAO {
+    public static ArrayList<Product> getProductInDb() {
+        Connection conn = null;
+        ArrayList<Product> productsList = new ArrayList<>();
+
+        try {
+            String url = "jdbc:mysql://localhost/mikencobrand";
+            String USER_NAME = "root";
+            String PASSWORD = "tuananhdeptrai";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, USER_NAME, PASSWORD);
+            String query = "select * from product";
+            Statement stmt = null;
+            try {
+                PreparedStatement pre = conn.prepareStatement(query);
+                ResultSet rs = pre.executeQuery();
+
+                while (rs.next()) {
+                    Product product = new Product();
+                    product.setProductId(rs.getInt("productId"));
+                    product.setCategoryId(rs.getInt("categoryId"));
+                    product.setQuantity(rs.getInt("quantity"));
+                    product.setPrice(rs.getInt("price"));
+                    product.setDescription(rs.getString("description"));
+                    product.setNameProduct(rs.getString("nameProduc"));
+                    product.setImgMain(rs.getString("imgMain"));
+                    product.setImg1(rs.getString("img1"));
+                    product.setImg2(rs.getString("img2"));
+                    product.setImg3(rs.getString("img3"));
+                    product.setImg4(rs.getString("img4"));
+                    product.setSize_S(rs.getInt("Size_S"));
+                    product.setSize_M(rs.getInt("Size_M"));
+                    product.setSize_L(rs.getInt("Size_L"));
+                    product.setSize_XL(rs.getInt("Size_XL"));
+                    product.setSize_XXL(rs.getInt("Size_XXL"));
+                    productsList.add(product);
+                }
+
+
+//                while (rs.next()) {
+//                    String name = rs.getString("username");
+//                    System.out.println(name);
+//                }
+            } catch (SQLException e) {
+                throw new Error("Problem", e);
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new Error("Problem", e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return productsList;
+    }
+
+    public static ArrayList<Product> deleteProduct(int ProductId) {
+        Connection conn = null;
+        ArrayList<Product> productsList = new ArrayList<>();
+
+        try {
+            String url = "jdbc:mysql://localhost/mikencobrand";
+            String USER_NAME = "root";
+            String PASSWORD = "tuananhdeptrai";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, USER_NAME, PASSWORD);
+            String query = "DELETE FROM product WHERE productId = ?;";
+            Statement stmt = null;
+            try {
+                PreparedStatement pre = conn.prepareStatement(query);
+                pre.setInt(1, ProductId);
+                int rs = pre.executeUpdate();
+
+//                while (rs.next()) {
+//                    Product product = new Product();
+//                    product.setProductId(rs.getInt("productId"));
+//                    product.setCategoryId(rs.getInt("categoryId"));
+//                    product.setQuantity(rs.getInt("quantity"));
+//                    product.setPrice(rs.getInt("price"));
+//                    product.setDescription(rs.getString("description"));
+//                    product.setNameProduct(rs.getString("nameProduc"));
+//                    product.setImgMain(rs.getString("imgMain"));
+//                    product.setImg1(rs.getString("img1"));
+//                    product.setImg2(rs.getString("img2"));
+//                    product.setImg3(rs.getString("img3"));
+//                    product.setImg4(rs.getString("img4"));
+//                    product.setSize_S(rs.getInt("Size_S"));
+//                    product.setSize_M(rs.getInt("Size_M"));
+//                    product.setSize_L(rs.getInt("Size_L"));
+//                    product.setSize_XL(rs.getInt("Size_XL"));
+//                    product.setSize_XXL(rs.getInt("Size_XXL"));
+//                    productsList.add(product);
+//                }
+
+
+//                while (rs.next()) {
+//                    String name = rs.getString("username");
+//                    System.out.println(name);
+//                }
+            } catch (SQLException e) {
+                throw new Error("Problem", e);
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new Error("Problem", e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return productsList;
+    }
+
+    public static ArrayList<CategoryModel> loadCategory() {
+        Connection conn = null;
+        ArrayList<CategoryModel> categoryList = new ArrayList<>();
+
+        try {
+            String url = "jdbc:mysql://localhost/mikencobrand";
+            String USER_NAME = "root";
+            String PASSWORD = "tuananhdeptrai";
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, USER_NAME, PASSWORD);
+            String query = "select * from category";
+            Statement stmt = null;
+            try {
+                PreparedStatement pre = conn.prepareStatement(query);
+                ResultSet rs = pre.executeQuery();
+
+                while (rs.next()) {
+                    CategoryModel categoryModel = new CategoryModel();
+                    categoryModel.setCategoryID(rs.getInt("categoryId"));
+                    categoryModel.setNameCategory(rs.getString("nameCategory"));
+                    categoryModel.setImg_Category(rs.getString("Img_Category"));
+                    categoryList.add(categoryModel);
+                }
+
+
+//                while (rs.next()) {
+//                    String name = rs.getString("username");
+//                    System.out.println(name);
+//                }
+            } catch (SQLException e) {
+                throw new Error("Problem", e);
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new Error("Problem", e);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return categoryList;
+    }
+}
+
